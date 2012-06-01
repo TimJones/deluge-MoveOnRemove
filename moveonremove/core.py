ed95d0ef957ec2756af33e70fef70152a601bc49
Tim Jones <me+deluge@prototim.com>
2012-06-01 18:49:59 +0200

    Added basic event handlers from torrent removal. Mostly just error checking and data gathering.
    Signed-off-by: Tim Jones <me+deluge@prototim.com>

--- moveonremove/core.py
+++ moveonremove/core.py
@@ -37,6 +37,8 @@
 #    statement from all source files in the program, then also delete it here.
 #
 
+import os
+
 from deluge.log import LOG as log
 from deluge.plugins.pluginbase import CorePluginBase
 import deluge.component as component
@@ -49,14 +51,56 @@ DEFAULT_PREFS = {
 
 class Core(CorePluginBase):
     def enable(self):
+        self.torrents_to_move = {}
         self.config = deluge.configmanager.ConfigManager("moveonremove.conf", DEFAULT_PREFS)
-        self.move_to = self.config["move_to"]
+        component.get("EventManager").register_event_handler("PreTorrentRemovedEvent", self.on_pre_torrent_removed)
+        component.get("EventManager").register_event_handler("TorrentRemovedEvent", self.on_torrent_removed)
 
     def disable(self):
-        self.config["move_to"] = self.move_to
+        self.torrents_to_move.clear()
+        component.get("EventManager").deregister_event_handler("PreTorrentRemovedEvent", self.on_pre_torrent_removed)
+        component.get("EventManager").deregister_event_handler("TorrentRemovedEvent", self.on_torrent_removed)
 
     def update(self):
         pass
+
+    def on_pre_torrent_removed(self, torrent_id):
+        """Grab all the torrent information needed to delete later"""
+        log.debug("MoveOnRemove: PreTorrentRemove " + torrent_id)
+
+        info_keys = ["save_path", "move_on_completed", "move_on_completed_path"]
+
+        if not torrent_id in component.get("TorrentManager").torrents:
+            log.error("MoveOnRemove: Cannot retrive torrent details")
+            return
+
+        self.torrents_to_move[torrent_id] = component.get("TorrentManager").torrents[torrent_id].get_status(info_keys)
+
+    def on_torrent_removed(self, torrent_id):
+        """Verify settings before doing anything"""
+        log.debug("MoveOnRemove: TorrentRemoved " + torrent_id)
+        move_to_path = self.config["move_to"]
+
+        if move_to_path == "":
+            log.error("MoveOnRemove: No path specified. Move aborted")
+            return
+
+        if not os.path.isdir( move_to_path ):
+            log.error("MoveOnRemove: Path '" + move_to_path + "' is invalid. Move aborted")
+            return
+
+        if not torrent_id in self.torrents_to_move:
+            log.error("MoveOnRemove: Torrent '" + torrent_id + "' info not found. Move aborted")
+            return
+
+        info = self.torrents_to_move[torrent_id]
+        path = info["move_on_completed_path"] if info["move_on_completed"] else info["save_path"]
+
+        if not os.path.exists( path ):
+            log.error("MoveOnRemove: Cannot find '" + path + "'. Move aborted")
+            return
+
+        log.error("MoveOnRemove: Going to move '" + path + "' to '" + move_to_path + "'")
 
     @export
     def set_config(self, config):
